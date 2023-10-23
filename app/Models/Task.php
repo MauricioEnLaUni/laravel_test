44c1629dd10baad883547e5948ff10284a414549
MauricioEnLaUni <210804@utags.edu.mx>
2023-10-23 00:42:39 -0600

    Added Models

---
+++ app/Models/Task.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Model;
+
+class Task extends Model
+{
+    use HasFactory;
+
+    protected $guarded = [];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function board()
+    {
+        return $this->belongsTo(Board::class);
+    }
+}
