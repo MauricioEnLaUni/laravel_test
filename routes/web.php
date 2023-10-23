13628166cca450dbc4859a239282f98698ce8877
MauricioEnLaUni <210804@utags.edu.mx>
2023-10-23 02:11:54 -0600

    Added catch all route to crud

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/{any?}', function ($any = null) {
+    if ($any && !str_starts_with($any, 'admin')) {
+        return redirect('/admin/');
+    } elseif(!$any) {
+        return redirect("/admin");
+    }
+})->where('any', '.*');
